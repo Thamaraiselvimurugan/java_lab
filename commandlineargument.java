d8dce80dd3ade298220037f956fe6819a8a68d50
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-08-08 14:12:09 +0530

    Add files via upload

---
+++ commandlineargument.java
@@ -0,0 +1,19 @@
+//command-line argument
+
+	class Exp66{  
+
+	public static void main(String args[]){  
+
+	  
+
+	for(int i=0;i<args.length;i++)  {
+
+	System.out.println(args[i]);  
+
+	  
+
+	}  
+
+	}  
+
+}
---
+++ ex50.java
@@ -0,0 +1,13 @@
+//save as ex50.java  
+
+package mypack;  
+
+public class ex50{  
+
+ public static void main(String args[]){  
+
+    System.out.println("Welcome to package");  
+
+   }  
+
+} 
