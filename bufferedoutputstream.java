af845866540438bc6cb637707ff917c249fb9955
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-08-22 21:22:12 +0530

    Add files via upload

---
+++ bufferedoutputstream.java
@@ -0,0 +1,31 @@
+import java.io.*;  
+
+	
+
+public class Exp146 {
+
+	public static void main(String args[])throws Exception{    
+
+	     FileOutputStream fout=new FileOutputStream("Exp146.txt");    
+
+	     BufferedOutputStream bout=new BufferedOutputStream(fout);    
+
+	     String s="Sachin... Sachin...";    
+
+	     byte b[]=s.getBytes();    
+
+	     bout.write(b);    
+
+	     bout.flush();    
+
+	     bout.close();    
+
+	     fout.close();    
+
+	     System.out.println("success");    
+
+	}    
+
+	  
+
+}
