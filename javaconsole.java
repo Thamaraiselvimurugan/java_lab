ad485250cfec5db9ee4e5deb20229fb7f2f8672e
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-09-08 22:26:32 +0530

    Add files via upload

---
+++ javaconsole.java
@@ -0,0 +1,25 @@
+//Java Console Example
+
+
+
+	import java.io.Console;
+
+public class Exp152 {
+
+	 
+
+ 
+
+		public static void main(String args[]){    
+
+			Console c=System.console();    
+
+			System.out.println("Enter your name: ");    
+
+			String n=c.readLine();    
+
+			System.out.println("Welcome "+n);    
+
+			}    
+
+}
