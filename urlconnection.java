643c43d2b2887e7802fbdfd46e7f9822be393cfa
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-09-09 19:35:22 +0530

    Add files via upload

---
+++ urlconnection.java
@@ -0,0 +1,33 @@
+import java.io.*;  
+
+	import java.net.*;  
+
+	
+
+public class Exp165 {
+
+	public static void main(String[] args){  
+
+	try{  
+
+	URL url=new URL("www.google.com");  
+
+	URLConnection urlcon=url.openConnection();  
+
+	InputStream stream=urlcon.getInputStream();  
+
+	int i;  
+
+	while((i=stream.read())!=-1){  
+
+	System.out.print((char)i);  
+
+	}  
+
+	}catch(Exception e){System.out.println(e);}  
+
+	}  
+
+	  
+
+}
